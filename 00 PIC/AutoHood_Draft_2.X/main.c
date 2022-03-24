4787b9226028e05d50932c921acd12f7bf5a6ead
thulsonASU <thulson@asu.edu>
2022-03-24 11:28:28 -0700

    testing branch compat

--- 00 PIC/AutoHood_Draft_2.X/main.c
+++ 00 PIC/AutoHood_Draft_2.X/main.c
@@ -7,7 +7,7 @@ void main(void)
 
     while (1)
     {
-        // Breadboard System
+        // Breadboard System test
         // Add your application code
         LED6_Toggle();
         __delay_ms(500);
