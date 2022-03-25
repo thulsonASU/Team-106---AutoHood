f633898e855fac21a8282f642401e7dd36194b8c
thulsonASU <thulson@asu.edu>
2022-03-25 13:25:39 -0700

    Breadboard Test One ACTUATOR

--- 00 PIC/AutoHood_Draft_2.X/main.c
+++ 00 PIC/AutoHood_Draft_2.X/main.c
@@ -6,6 +6,34 @@ void main(void)
    
     SYSTEM_Initialize();
 
+    // Power Up LED
+    
+    LED_A_SetHigh();
+    __delay_ms(200);
+    
+    LED_H_SetHigh();
+    __delay_ms(200);
+    
+    LED_M_SetHigh();
+    __delay_ms(200);
+    
+    LED_P_SetHigh();
+    __delay_ms(200);
+    
+    // Power Down LED
+    
+    LED_A_SetLow();
+    __delay_ms(200);
+    
+    LED_H_SetLow();
+    __delay_ms(200);
+    
+    LED_M_SetLow();
+    __delay_ms(200);
+    
+    LED_P_SetLow();
+    __delay_ms(200);
+    
     // MICHAEL SUBSYSTEM ANNEMOMETER
     float Voltage;
     float VoltageActual;
@@ -20,20 +48,21 @@ void main(void)
     {
         // Breadboard System test branch
         // Add your application code
-        if(RA7 == 0)
+        if(BTN_D_GetValue() == 0)
         {    
-            
+            LED_M_SetHigh();
             __delay_ms(200);
+            
             motorCW(100,5);
             motorCCW(100,5);
             
+            LED_M_SetLow();
+            __delay_ms(200);
         }
         else
         {
-            LED6_Toggle();
-            __delay_ms(500);
+            
             adc_result_t convertedValue;
-   
    
             convertedValue = ADCC_GetSingleConversion(ANA);
             Voltage = (float) convertedValue;
@@ -41,15 +70,15 @@ void main(void)
             VoltageActual = (Resolution * Voltage);
    
             if (VoltageActual>70) //mV
-            LED7_Toggle();
+            {
+                LED_A_SetHigh();
+            }
             else
             {
-            LED7_Toggle();
+                LED_A_SetLow();
             }
-           
         }
                 // MICHAEL ANNEMOMETER         
-    
     }
 }
 /**
